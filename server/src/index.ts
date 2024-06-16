6897138fed1175c505f6a2457f194ebad835d817
ericliu22 <ericliu108@gmail.com>
2024-06-15 23:30:38 -0400

    Added skeleton api server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+import { Elysia } from "elysia";
+import { config } from "dotenv";
+
+//NECESSARY TO IMPORT ENV FILES
+config();
+
+const app = new Elysia()
+
+app.get("/api/v1/", () => {
+	return "This is the api for tiktok app";
+})
+
+app.post("/api/v1/echo", ({ body }) => {
+	return `${ body }`;
+});
+
+app.listen(process.env.PORT ?? 8080, () => {
+	console.log(`Server is running on port ${app.server?.port}...`);
+})
