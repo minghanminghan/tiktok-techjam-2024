b4aca64606a9cd028e7969bdff44a842da76ca8e
ericliu22 <ericliu108@gmail.com>
2024-06-18 23:20:29 -0400

    Added authentication functions
    -Added user and song schemas
    -Added packages bcrypt and @types/bcrypt

---
+++ server/src/lib/auth.ts
@@ -0,0 +1,103 @@
+import bcrypt from "bcrypt";
+import { User } from "./schemas";
+
+/**
+ * The identifier can be either email or username
+ * The password is plaintext bruh
+ */
+interface LoginInput {
+	identifier: string,
+	password: string,
+}
+
+async function comparePassword(
+	password: string,
+	hash: string,
+	salt: string
+): Promise<boolean>{
+	const inputHash = await bcrypt.hash(password, salt);
+	const passwordMatch = await bcrypt.compare(inputHash, hash)
+	if (!passwordMatch) {
+		return false;
+	}
+	return true;
+}
+
+/**
+ * @param LoginInput
+ * An interface with either username or password as identifier and the plain-text password for the user
+ * @returns
+ * Returns the authentication token. Don't know what that looks like yet
+ * @example
+	const login: LoginInput = {identifier: username, password: password}
+	const authToken = await loginUser(login);
+ */
+async function loginUser({ identifier, password }: LoginInput): Promise<string> {
+	const user = await User.findOne({
+		$or: [{ username:identifier }, { email:identifier }]
+	});
+
+	if (!user) {
+		throw new Error("User not found");
+	}
+
+	const passwordMatch = await comparePassword(password, user.password, user.salt);
+	if (!passwordMatch) {
+		throw new Error("Password does not match");
+	}
+	//@TODO: Include authentication token generation
+	return "";
+}
+
+/**
+ * @param
+ * password - plaintext eleg
+ * @throws
+ * username email in use bullshit 
+ * @returns
+ * The auth token from loginUser
+ */
+async function registerUser (
+	username: string, 
+	password: string, 
+	email: string
+): Promise<string> {
+	const avaliable = await checkAvaliability(username, email);
+	if (!avaliable) {
+		throw new Error("Username or email already in use!");
+	}
+
+	const [salt, hash] = await generateHash(password);
+	const newUser = new User({
+		username: username,
+		password: hash,
+		salt: salt,
+		email: email,
+		liked: [],
+		disliked: [],
+	});
+	await newUser.save();
+
+	const login: LoginInput = {identifier: username, password: password}
+	return await loginUser(login);
+}
+
+async function generateHash(password: string): Promise<[string, string]> {
+	const salt = await bcrypt.genSalt(10);
+	const hash = await bcrypt.hash(password, salt);
+	return [salt, hash];
+}
+
+async function checkAvaliability(username: string, email: string): Promise<boolean> {
+	const user = await User.findOne({
+		$or: [{ username:username }, { email:email }]
+	});
+	if (user) return false;
+	else return true;
+}
+
+export {
+	type LoginInput,
+	loginUser,
+	registerUser,
+}
--- server/src/lib/schemas.ts
+++ server/src/lib/schemas.ts
@@ -1,3 +1,57 @@
-import ("mongoose");
+import { Schema, model } from "mongoose";
 
+enum SongType {
+	spotify,
+	uploaded
+}
+const SongSchema = new Schema({
+	title: String,
+	artist: String,
+	type: SongType,
+	release: Number,
+});
 
+/**
+ * A Schema that represents a song
+ * @param title: string
+ * 	The song title
+ * @param artist: string 
+ *   	The artist who made the song
+ * @param type: SongType
+ *   	The enum that describes who the data should be stored
+ * @param release: Number
+ *   	The year that the song was release in
+ */
+const Song = model("Song", SongSchema);
+
+const UserSchema = new Schema({
+	username: { type: String, required: true, unique: true },
+	password: { type: String, required: true },
+	salt: { type: String, required: true },
+	email: { type: String, required: true, unique: true },
+	liked: { type: [Song], required: true },
+	disliked: { type: [Song], required: true },
+})
+
+/**
+ * User
+ * @param username: string
+ *   	The username
+ * @param password: string
+ *   	The stored hash for the user
+ * @param salt: string
+ *   	The input used to generate the hash
+ * @param email: string
+ *   	user email
+ * @param liked: [Song]
+ *   	List of songs that the user has swiped right on
+ * @param disliked: [Song]
+ *   	List of songs that the user has swiped left on
+ */
+const User = model("User", UserSchema);
+
+export {
+	Song,
+	User,
+	SongType
+}
